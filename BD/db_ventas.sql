d693fe9af4ecc60d8fad45058b3dd61b8cd31609
enrique Caballero Canchaya <enrique.caballero0602@gmail.com>
2022-02-14 08:31:48 -0500

    actualizando la bd

--- BD/db_ventas.sql
+++ BD/db_ventas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-02-2022 a las 02:15:16
+-- Tiempo de generación: 14-02-2022 a las 14:27:00
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 7.3.33
 
@@ -39,6 +39,15 @@ CREATE TABLE `articulo` (
   `fechaCaptura` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
+--
+-- Volcado de datos para la tabla `articulo`
+--
+
+INSERT INTO `articulo` (`id_producto`, `id_categoria`, `id_imagen`, `id_usuario`, `nombre`, `descripcion`, `cantidad`, `precio`, `fechaCaptura`) VALUES
+(1, 2, 0, 6, 'ENRIQUE', 'ddd', 1, 213, '2022-02-09'),
+(3, 2, 11, 6, 'FRESA', 'FRIO RICO', 2, 15, '2022-02-11'),
+(4, 5, 12, 6, 'ADIDAS', 'POLO DE ALGODON 100% PERUANO', 2, 1500, '2022-02-11');
+
 -- --------------------------------------------------------
 
 --
@@ -57,7 +66,9 @@ CREATE TABLE `categoria` (
 --
 
 INSERT INTO `categoria` (`id_categoria`, `id_usuario`, `nombreCategoria`, `fechaCaptura`) VALUES
-(2, 6, 'HELADOS', '2022-02-03');
+(2, 6, 'HELADOS', '2022-02-03'),
+(4, 6, 'AUTOS', '2022-02-08'),
+(5, 6, 'POLO', '2022-02-11');
 
 -- --------------------------------------------------------
 
@@ -75,6 +86,15 @@ CREATE TABLE `clientes` (
   `telefono` varchar(15) COLLATE utf8_spanish2_ci DEFAULT NULL,
   `rfc` varchar(200) COLLATE utf8_spanish2_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id_cliente`, `id_usuario`, `nombre`, `apellido`, `direccion`, `email`, `telefono`, `rfc`) VALUES
+(1, 6, 'luis', 'EEE', 'E', 'EEEgg', '555', 'EEE'),
+(3, 6, 'ENRIQUE', 'CABALLERO', 'SS', 'SS', '5555', 'SSS'),
+(4, 6, 'Andy', 'POLO', 'LOS JIRASOLES', 'ELPAPURI@gamil.com', '99955211', 'los jupiter');
 
 -- --------------------------------------------------------
 
@@ -95,7 +115,16 @@ CREATE TABLE `imagenes` (
 --
 
 INSERT INTO `imagenes` (`id_imagen`, `id_categoria`, `nombre`, `ruta`, `fechaSubida`) VALUES
-(2, 0, 'itachi.jpg', '', '2022-02-04');
+(2, 0, 'itachi.jpg', '', '2022-02-04'),
+(3, 0, 'design.png', '../../archivos/design.png', '2022-02-08'),
+(4, 2, 'design.png', '../../archivos/design.png', '2022-02-08'),
+(5, 4, 'toyota.jpg', '../../archivos/toyota.jpg', '2022-02-08'),
+(6, 4, 'toyota.jpg', '../../archivos/toyota.jpg', '2022-02-08'),
+(7, 4, 'toyota.jpg', '../../archivos/toyota.jpg', '2022-02-09'),
+(8, 4, 'toyota.jpg', '../../archivos/toyota.jpg', '2022-02-09'),
+(9, 2, 'luisenrique.jpg', '../../archivos/luisenrique.jpg', '2022-02-09'),
+(11, 2, 'itachi.jpg', '../../archivos/itachi.jpg', '2022-02-11'),
+(12, 5, 'pique_acero.jpg', '../../archivos/pique_acero.jpg', '2022-02-11');
 
 -- --------------------------------------------------------
 
@@ -117,12 +146,9 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id_usuario`, `nombre`, `apellido`, `email`, `password`, `fechaCaptura`) VALUES
-(1, 'ENRIQUE', 'sfafd', 'ebe@kdkd.com', 'b978a979693093a7405a6791380a8f19415a9bec', '2022-01-30'),
-(2, 'ENRIQUE', 'sfafd', 'luisenrique@gmail.com', 'b978a979693093a7405a6791380a8f19415a9bec', '2022-01-30'),
-(3, 'ENRIQUE', 'sfafd', 'luisenrique@gmail.com', 'b978a979693093a7405a6791380a8f19415a9bec', '2022-01-30'),
 (4, 'Pipa del 8', 'Ursa Phaton lancer', 'support@hotmail.com', '3b3eb0498fef7fbfca0cf08d86a1815d62ba18ea', '2022-01-31'),
-(5, 'admin', 'admin', 'admin@hotmail.com', 'd033e22ae348aeb5660fc2140aec35850c4da997', '2022-01-31'),
-(6, 'admin', 'admin', 'admin@gmail.com', 'f865b53623b121fd34ee5426c792e5c33af8c227', '2022-01-31');
+(6, 'admin', 'admin', 'admin@gmail.com', 'f865b53623b121fd34ee5426c792e5c33af8c227', '2022-01-31'),
+(10, 'PEPE', 'PEPE', 'PEPE', '3d72e9967fe1a33d2ba707551bec221eca7ebebf', '2022-02-12');
 
 -- --------------------------------------------------------
 
@@ -138,6 +164,16 @@ CREATE TABLE `ventas` (
   `precio` float DEFAULT NULL,
   `fechaCompra` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `ventas`
+--
+
+INSERT INTO `ventas` (`id_venta`, `id_cliente`, `id_producto`, `id_usuario`, `precio`, `fechaCompra`) VALUES
+(1, 4, 1, 6, 15, '2022-02-11'),
+(1, 1, 4, 6, 1500, '2022-02-11'),
+(1, 4, 4, 6, 1500, '2022-02-11'),
+(2, 4, 4, 6, 1500, '2022-02-12');
 
 --
 -- Índices para tablas volcadas
@@ -181,31 +217,31 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `articulo`
 --
 ALTER TABLE `articulo`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `imagenes`
 --
 ALTER TABLE `imagenes`
-  MODIFY `id_imagen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_imagen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
